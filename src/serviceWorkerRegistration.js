49ad62b4c87271665e99a0e01f56c24c2147ef6d
NickBritoDev <nicolasbcruz@gmail.com>
2023-12-15 10:50:53 -0300

    fix: testando

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -1,14 +1,10 @@
-export function registerServiceWorker () {
+export async function register () {
   if ('serviceWorker' in navigator) {
-    window.addEventListener('load', async () => {
-      try {
-        const registration = await navigator.serviceWorker.register('/service-worker.js')
-        console.log('Service Worker registered:', registration)
-      } catch (error) {
-        console.error('Service Worker registration failed:', error)
-      }
-    })
+    try {
+      const registration = await navigator.serviceWorker.register('/service-worker.js')
+      console.log('Service Worker registered:', registration)
+    } catch (error) {
+      console.error('Service Worker registration failed:', error)
+    }
   }
 }
-
-registerServiceWorker()
