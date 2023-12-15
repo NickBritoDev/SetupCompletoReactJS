0159b4f45f99034377e7b309fb47f09015ca79d3
NickBritoDev <nicolasbcruz@gmail.com>
2023-12-15 11:27:27 -0300

    fix: teste

--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),
     VitePWA({
       manifest: {
-        // Configurações do manifest
-        name: 'Nome da Sua PWA',
+        name: 'Setup Completo ReactJS',
         short_name: 'PWA',
         start_url: '/SetupCompletoReactJS/',
         display: 'standalone',
         theme_color: '#ffffff',
         icons: [
           {
-            // Configurações do ícone
-            src: 'caminho/do/seu/icone.png',
+            src: './public/react.png',
             sizes: '192x192',
             type: 'image/png'
           }
