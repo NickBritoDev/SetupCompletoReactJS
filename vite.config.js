448fbc6660fe517cf13dd8e0e98aa85f7587d093
NickBritoDev <nicolasbcruz@gmail.com>
2023-12-15 11:10:52 -0300

    fix: pwa vite

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,21 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [react(),
     VitePWA({
-      registerType: 'autoUpdate',
-      workbox: {
-        clientsClaim: true,
-        skipWaiting: true
+      manifest: {
+        // Configurações do manifest
+        name: 'Nome da Sua PWA',
+        short_name: 'PWA',
+        start_url: '/SetupCompletoReactJS/',
+        display: 'standalone',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            // Configurações do ícone
+            src: 'caminho/do/seu/icone.png',
+            sizes: '192x192',
+            type: 'image/png'
+          }
+        ]
       }
     })
   ],
